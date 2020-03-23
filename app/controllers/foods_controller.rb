8c301dbfaeb36e8036254c765d441f6b9a63d1c1
Happy8-8 <gafonji@gmail.com>
2020-03-23 02:18:40 +0000

    setup heroku

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,21 @@
+class FoodsController < ApplicationController
+  def index
+    @foods = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    Food.create(food_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :description)
+  end
+
+end
